156b40ddd8ace71cf78f79a5daf5b7678e84f4c7
Hasier Rodriguez <hasier@jinnapp.com>
2015-04-15 22:10:34 +0100

    Add tropo demo response

--- app/api.py
+++ app/api.py
@@ -1,6 +1,7 @@
 __author__ = 'hasier'
 
-from flask import Response
+from flask import Response, jsonify, request
+from tropo import Session
 
 from app import app
 
@@ -8,3 +9,11 @@ from app import app
 @app.route('/')
 def root():
     return Response(status=200)
+
+
+@app.route('/tropo')
+def tropo():
+    session = Session(request.get_json(force=True))
+    return jsonify({
+        "tropo": [{"say": {"value": "It works!"}}]
+    })
